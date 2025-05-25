4a3812a420f6a56797a33716983098b3904310e2
Yuri Melo <yurifullstack@gmail.com>
2025-05-24 22:08:50 -0300

    List logs in commits history

---
+++ lib/git_beholder/git_log.ex
@@ -0,0 +1,40 @@
+defmodule GitBeholder.GitLog do
+  @default_limit 10
+
+  def list_logs(repo_path, limit \\ @default_limit) do
+    if File.dir?(Path.join(repo_path, ".git")) do
+      args = [
+        "log",
+        "-n",
+        "#{limit}",
+        "--pretty=format:'%H|%an|%ad|%s'",
+        "--date=short"
+      ]
+
+      case System.cmd("git", args, cd: repo_path, stderr_to_stdout: true) do
+        {output, 0} ->
+          commits =
+            output
+            |> String.trim()
+            |> String.split("\n", trim: true)
+            |> Enum.map(fn line ->
+              [hash, author, date, message] = String.split(line, "|", parts: 4)
+
+              %{
+                hash: hash,
+                author: author,
+                date: date,
+                message: message
+              }
+            end)
+
+          {:ok, commits}
+
+        {error_msg, _exit_code} ->
+          {:error, error_msg}
+      end
+    else
+      {:error, "Not a valid Git repository"}
+    end
+  end
+end
