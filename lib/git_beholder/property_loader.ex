4741cb851def590e870263f8ab081ee11498f8e6
Yuri Melo <yurifullstack@gmail.com>
2025-06-14 19:53:54 -0300

    Add spec attribute

--- lib/git_beholder/property_loader.ex
+++ lib/git_beholder/property_loader.ex
@@ -1,22 +1,24 @@
 defmodule GitBeholder.PropertyLoader do
-    @json_path Path.join(:code.priv_dir(:git_beholder), "properties.json")
+  @json_path Path.join(:code.priv_dir(:git_beholder), "properties.json")
 
-    def get_property(key) when is_binary(key) do
-      with {:ok, body} <- File.read(@json_path),
-           {:ok, props} <- Jason.decode(body) do
-        Map.get(props, key)
-      else
-        _ -> nil
-      end
-    end
-
-    def get_root_directory do
-      case get_property("rootDirectory") do
-        dir when is_binary(dir) and dir != "" ->
-          dir
-
-        _ ->
-          "./repositories"
-      end
+  @spec get_property(binary()) :: any()
+  def get_property(key) when is_binary(key) do
+    with {:ok, body} <- File.read(@json_path),
+         {:ok, props} <- Jason.decode(body) do
+      Map.get(props, key)
+    else
+      _ -> nil
     end
   end
+
+  @spec get_root_directory() :: binary()
+  def get_root_directory do
+    case get_property("rootDirectory") do
+      dir when is_binary(dir) and dir != "" ->
+        dir
+
+      _ ->
+        "./repositories"
+    end
+  end
+end
